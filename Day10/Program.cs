a3d7a57b63c3de977bfed6e60d333e192912de33
phamtien98 <35487979+phamtien98@users.noreply.github.com>
2022-02-25 00:47:01 +0700

    edit connectionString

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IStudentService, StudentService>();
-builder.Services.AddDbContext<StudentContext>(options => options.UseSqlServer("name=ConnectionStrings:StudentConnection"));
+builder.Services.AddDbContext<StudentContext>(options => options.UseSqlServer("name=ConnectionStrings:ProductConnection"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
